cc27e30f6f9e5518de1060a64317de37fd2e9a90
Justusschl <49748349+Justusschl@users.noreply.github.com>
2020-06-02 09:05:08 +0200

    a

--- Code/tps/main/fm_model.py
+++ Code/tps/main/fm_model.py
@@ -515,7 +515,7 @@ def model(t, y, p, *args):
        astblock*fluxg,
         #postsyn
        fluxp,
-       ((-1 / p.F * (INaGp + INaLp + 3 * IpumpP + IAMPA2)) - 3 * p.F / INCXp), #+ JAMPA #works: - p.F * INCXp
+        ((-1 / p.F * (INaGp + INaLp + 3 * IpumpP + IAMPA2)) - p.F * INCXp), #+ JAMPA #works: - p.F * INCXp
        (-1 / p.F * (IKGp + IKLp - 2 * IpumpP) - JKClp),
        (1 / p.F * (IClGp + IClLp) - JKClp),
        (-1 / 2 / p.F) * (ICaGp + ICaLp - INCXp),
